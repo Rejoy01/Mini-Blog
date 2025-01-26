e875819333835c04ce7dcd144f7b632e1f886bf4
Rejoy01 <rejoywilson65@gmail.com>
2025-01-26 15:52:39 +0530

    Moderation Service

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,25 +1,10 @@
 import React , {useState , useEffect} from 'react'
 import axios from 'axios'
 
-const CommentList = ({postId}) => {
-    
-    const[comments,setComment] = useState([])
 
-    const fetchData = async () => {
-
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-        setComment(res.data)
-        // console.log(res.data);
-
-    }
-
-    useEffect(()=>{      
-        fetchData()
-        // 
-        
-    },[])
-
-    const renderedComments = comments.map(comments =>{
+const CommentList = ({comments}) => {
+   
+    const renderedComments = comments?.map(comments =>{
         return <li  key={comments.id}>{comments.content}</li>
     })
 
@@ -31,3 +16,22 @@ const CommentList = ({postId}) => {
 }
 
 export default CommentList
+// using query service we don't need to make get requests for each comment . Query servcie will provide all the info that recevide from event bus
+
+ //After evnt bus and query service we get Post And comment details from query service no need to fetch comment details using PostId
+    // we can delete fetch method inside the comment list
+    // const[comments,setComment] = useState([])
+
+    // const fetchData = async () => {
+
+    //     const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+    //     setComment(res.data)
+    //     // console.log(res.data);
+
+    // }
+
+    // useEffect(()=>{      
+    //     fetchData()
+    //     // 
+        
+    // },[])
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -9,28 +9,47 @@ const PostList = () => {
 
     const [posts , setPosts] = useState("{}")
     const fetchPosts = async () =>{
-        const res = await Api.get('/posts')
-        
+        // before event bus and query services
+        // const res = await Api.get('/posts')
+        const res = await axios.get('http://localhost:4002/posts')
         setPosts(res.data)
-
+        
+        
     }
     useEffect(()=>{
         fetchPosts();
+        console.log(posts);
     },[]);
 
  const renderedPosts = Object.values(posts).map(post => {
-    return( 
+  
+      return( 
     <div className="card" style={{width:'30%'}} key={post.id}>
 
-        <div className="card-body" >
+        <div className="card-body" key={post.id}>
             <h3>{post.title}</h3>
-            <CommentList postId={post.id}/>
+            <CommentList comments={post.comments}/>
             <CommentCreate postId={post.id}/>
         </div>
         
 
     </div>
     )
+    //before event bust we need to pass post.id to fecth comment 
+    //After evnt bus and query service we get Post And comment details from query service no need to fetch comment details using PostId
+    // we can delete fetch method inside the comment list
+    // return( 
+    // <div className="card" style={{width:'30%'}} key={post.id}>
+
+    //     <div className="card-body" key={post.id}>
+    //         <h3>{post.title}</h3>
+    //         <CommentList postId={post.id}/>
+    //         <CommentCreate postId={post.id}/>
+    //     </div>
+        
+
+    // </div>
+    // )
  })
     
   return (
