20dff52b0d17168e301b86970c721862a424a34b
Rejoy01 <rejoywilson65@gmail.com>
2025-04-06 14:11:51 +0530

    containerized all microservices

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,6 +13,7 @@ App.post('/events',(req,res)=>{
     
 
     axios.post('http://localhost:4000/events',event);
+    
     axios.post('http://localhost:4002/events',event).catch((err)=>{
         console.log(err.message);
         
